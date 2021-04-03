5fa689d541225b9305969efa514be546c8311547
Pedro <pedrovianaasking@gmail.com>
2021-04-02 22:00:43 -0300

    Half Mechanic deployed

--- PADDLE_GAME/class.hpp
+++ PADDLE_GAME/class.hpp
@@ -7,6 +7,7 @@
 
 
 
+
 #define WINDOW_HEIGHT 500
 #define WINDOW_WIDTH 800
 #define FPS 30 
@@ -25,11 +26,14 @@ class Game
         SDL_Event event;
         SDL_Rect ball{ball.x=WINDOW_WIDTH/2-7, ball.y=WINDOW_HEIGHT/2,BALL_WIDTH, BALL_WIDTH};
         SDL_Rect paddle1{paddle1.x=0, paddle1.y=WINDOW_HEIGHT/2, 15,50};
-        SDL_Rect paddle2{paddle2.x=WINDOW_WIDTH-15, paddle2.y=WINDOW_HEIGHT/2, 15,50};
+        TTF_Font *font;
         int last_frame_time=0;
         float delta_time; 
         int time_to_wait;
-        int directionsXY[1];
+        float a[4]; 
+        bool ball_start;
+        bool ball_bounce;
+
         
     public:
         Game();
--- PADDLE_GAME/functions.hpp
+++ PADDLE_GAME/functions.hpp
@@ -4,6 +4,7 @@
 #include<cstdlib>
 #include <iostream>
 #include <SDL2/SDL.h>
+#include<SDL2/SDL_ttf.h>
 #include "class.hpp"
 
 
@@ -11,6 +12,8 @@
 
 Game::Game(){
 
+    a[0]=1.25; a[1]= 1.09; a[3]=0; a[4]=1.2;    
+    
     
 
     if(SDL_Init(SDL_INIT_EVERYTHING)!=0){
@@ -29,11 +32,26 @@ Game::Game(){
     if(!render){
         std::cout<<"SDL RENDER  ERROR"<<SDL_GetError();
         isRuning=false;}
+
+   if(TTF_Init()==-1){
+       std::cout<<"TTF ERROR: "<<TTF_GetError()<<std::endl;
+
+       isRuning=false;
+   }
+
+    font = TTF_OpenFont("font.ttf",16);
+    if(!font){
+        std::cout<<"TTF OPEN FONT ERRPR<"<<TTF_GetError()<<std::endl;
+    }
+
+
+
 }
 
 Game::~Game(){
     SDL_DestroyRenderer(render);
     SDL_DestroyWindow(window);
+    TTF_Quit();
     SDL_Quit();
 }
 
@@ -54,16 +72,13 @@ void Game::renderization(){
     SDL_SetRenderDrawColor(render, 255,255,255,255);
     SDL_RenderFillRect(render, &paddle1);
 
-    SDL_SetRenderDrawColor(render, 255,255,255,255);
-    SDL_RenderFillRect(render, &paddle2);
-
     SDL_RenderPresent(render);
 
 
 }
 
 void Game::update(){
-    
+     
     
     
 
@@ -73,24 +88,29 @@ void Game::update(){
         SDL_Delay(time_to_wait);
     }
 
-    delta_time = (SDL_GetTicks()- last_frame_time)/10.0F;
+    delta_time = (SDL_GetTicks()- last_frame_time)/13.0F;
     
     system("clear");
 
     std::cout<<"Delta time: "<<delta_time<<std::endl;
     std::cout<<"last frame time: "<<last_frame_time<<std::endl;
     std::cout<<"paddle one position y: "<<paddle1.y<<std::endl;
-    std::cout<<"paddle two position y : "<<paddle2.y<<std::endl;
     std::cout<<"paddle one position x: "<<paddle1.x<<std::endl;
-    std::cout<<"paddle two position x : "<<paddle2.x<<std::endl;
     std::cout<<"bal position x : "<<ball.x<<std::endl;
     std::cout<<"ball position y : "<<ball.y<<std::endl;
     last_frame_time = SDL_GetTicks();
+
+    if(paddle1.y<=0) paddle1.y=1;
+    if (paddle1.y>=WINDOW_HEIGHT-paddle1.h) paddle1.y= WINDOW_HEIGHT-paddle1.h;
+
+
 }
 
 
 
 void Game::process(){
+
+    float i=5;
 
     SDL_PollEvent(&event);
     
@@ -103,25 +123,45 @@ void Game::process(){
     
     case SDL_KEYDOWN:
 
-        if(paddle1.y<=0) paddle1.y=1;
-
         if(event.key.keysym.sym==SDLK_DOWN) paddle1.y+=3*delta_time;
-            
-        
-        if(paddle1.y>=WINDOW_HEIGHT) paddle1.y=WINDOW_HEIGHT-paddle1.h; 
-
+     
         if(event.key.keysym.sym==SDLK_UP) paddle1.y-=3*delta_time;
 
 
     case SDL_KEYUP: 
         if(event.key.keysym.sym==SDLK_SPACE){
-            ball.x+=1*delta_time*1.12;
+            ball_start = true;
         }        
         
         break;
        }
-        
+
+    if(ball_start){
+        ball.x+=4*delta_time;
+        if(ball.x>=WINDOW_WIDTH){
+            ball_start=false;
+            ball_bounce=true;
+            }
+        }
+    
+    if(ball_bounce){
+        ball.x-=5*delta_time;
+        if(ball.x<=paddle1.w && ball.y>=paddle1.y && ball.y>=paddle1.y-paddle1.h){
+            ball_start= true;
+            ball_bounce=false;
+        }
     }
+    
+    if(ball.x<=0){
+        ball.x=WINDOW_WIDTH/2-7;
+        ball.y=WINDOW_HEIGHT/2;
+        ball_start=false;
+        ball_bounce=false;
+    }
+
+    }
+
+    
 
 
 
